a241f4160536bdd78e162223bfccd572374f7006
sclary110 <sclary@icloud.com>
2021-05-24 10:46:22 -0400

    story update

--- src/Pages/Landing/Home/storyData.js
+++ src/Pages/Landing/Home/storyData.js
@@ -9,31 +9,31 @@ export const stories = [
 		review: 'THIS. I need this in my life. Such a great idea. AND A SOCIAL ASPECT!',
 		link: ''
 	},
-	{
-		name: 'Drew Wilson',
-		review: 'Love this concept. I’ll be using it for all my run commands.',
-		link: ''
-	},
-	{
-		name: 'Mark R.',
-		review: 'This is a great idea. So dope.',
-		link: ''
-	},
-	{
-		name: 'Arthur B.',
-		review: '🐐',
-		link: ''
-	},
+	// {
+	// 	name: 'Drew Wilson',
+	// 	review: 'Love this concept. I’ll be using it for all my run commands.',
+	// 	link: ''
+	// },
+	// {
+	// 	name: 'Mark R.',
+	// 	review: 'This is a great idea. So dope.',
+	// 	link: ''
+	// },
+	// {
+	// 	name: 'Arthur B.',
+	// 	review: '🐐',
+	// 	link: ''
+	// },
 	{
 		name: 'Bunmee V.',
 		review: 'Wow this is really nice and useful. A great idea!',
 		link: ''
 	},
-	{
-		name: 'Kristen P.',
-		review: 'Yeah, I want to use this. LOVE!',
-		link: ''
-	},
+	// {
+	// 	name: 'Kristen P.',
+	// 	review: 'Yeah, I want to use this. LOVE!',
+	// 	link: ''
+	// },
 	{
 		name: 'Evan J.',
 		review: 'Incredibly useful. This is awesome!',
